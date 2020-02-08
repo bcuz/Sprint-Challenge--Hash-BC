9a3b6e68f8681fa67f2a146844a930ac042e1273
adam <adam.nawroc@gmail.com>
2020-02-08 16:03:34 -0500

    trying stuff

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,8 +14,35 @@ def reconstruct_trip(tickets, length):
   hashtable = HashTable(length)
   route = [None] * length
 
-  """
-  YOUR CODE HERE
-  """
+  # if source is None, it's the first one
+  # if destination is None, it's the last one.
+
+  for i in range(len(tickets)):
+    hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
+
+  print(hashtable.storage)
+
+  # for i in range(len(hashtable.storage)):
+  #   if hashtable.storage[i] != None:
+  #     print(hashtable.storage[i].key, hash_table_retrieve(hashtable, hashtable.storage[i].key))
 
   return route
+
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+           ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+expected = ["LAX", "SFO", "BHM", "FLG", "XNA", "SAP",
+            "SLC", "PIT", "ORD", "NONE"]
+result = reconstruct_trip(tickets, 10)
