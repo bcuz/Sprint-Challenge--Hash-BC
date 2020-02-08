4d6a02b545733f5cd450b0c02bb3995919cf3d9b
adam <adam.nawroc@gmail.com>
2020-02-08 12:34:07 -0500

    storing stuff in a hash table

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -10,7 +10,17 @@ def get_indices_of_item_weights(weights, length, limit):
   ht = HashTable(16)
 
   for i in range(len(weights)):
-    print(weights[i], i)
+    hash_table_insert(ht, i, weights[i])
+
+  # it overwrites the weight, tho, if there's a dup
+  # as it should. like the advice is flipped..
+  print(ht.storage)
+
+  # for item in ht.storage:
+  #   current = item
+  #   while current is not None:
+  #     print(current.key, current.value)
+  #     current = current.next
 
   return None
 
