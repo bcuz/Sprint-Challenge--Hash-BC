b397b43fec1ebfa8f0b4a902d21367d4361e7e61
adam <adam.nawroc@gmail.com>
2020-02-10 20:36:38 -0500

    ex1 tests passing somehow

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -41,29 +41,32 @@ def get_indices_of_item_weights(weights, length, limit):
   total = 0
   values = [None] * 2
   while True:
-    if counter == len(ht.storage):
+    if len(weights) == 1:
+      return None
+
+    if counter == len(weights):
       counter2 += 1
       counter = counter2
       total = 0
-      values = [None] * 2
-      
+      values = [None] * 2      
     
     # if there are no nones left after the first 
     # if len(values) == 2:
 
     # print(counter)
-    if ht.storage[counter] != None:
-      if total == 0:
-        total = hash_table_retrieve(ht, ht.storage[counter].key)
-        values[0] = ht.storage[counter].key
-        # print(values)
-      else:
-        current = hash_table_retrieve(ht, ht.storage[counter].key)
+    # if ht.storage[counter] != None:
+    if total == 0:
+      total = hash_table_retrieve(ht, counter)
+      # print(total == 7, counter)
+      values[0] = counter
+      # print(values)
+    else:
+      current = hash_table_retrieve(ht, counter)
 
-        if (total + current) == limit:
-          values[1] = ht.storage[counter].key          
-          # total += hash_table_retrieve(ht, ht.storage[counter].key)          
-          break
+      if (total + current) == limit:
+        values[1] = counter          
+        # total += hash_table_retrieve(ht, ht.storage[counter].key)          
+        break
 
     counter += 1
 
@@ -72,7 +75,7 @@ def get_indices_of_item_weights(weights, length, limit):
     values[0] = values[1]
     values[1] = temp
 
-  return values
+  return (values[0], values[1])
 
   # print(ht.storage)
   # # for item in ht.storage:
@@ -91,6 +94,13 @@ weights_2 = [4, 4]
 answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
 print(answer_2)
 
-# weights_3 = [4, 6, 10, 15, 16]
-# answer_3 = get_indices_of_item_weights(weights_3, 5, 21)
-# print(answer_3)
+weights_3 = [4, 6, 10, 15, 16]
+answer_3 = get_indices_of_item_weights(weights_3, 5, 21)
+print(answer_3)
+
+weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+answer_4 = get_indices_of_item_weights(weights_4, 9, 7)
+print(answer_4)
+
+weights_1 = [9]
+answer_1 = get_indices_of_item_weights(weights_1, 1, 9)
